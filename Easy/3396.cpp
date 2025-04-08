eec9dbc6d794d9605cd381f86eae0f980b87df46
Gyan Prakash Rai <shanuraig@gmail.com>
2025-04-09 01:40:07 +0530

    Optimised 3396 easy

--- Easy/3396.cpp
+++ Easy/3396.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 class Solution {
     public:
+    //this approach is brute force 
     bool check (int start,vector<int> &nums ,int n){
         unordered_set<int> st;
         for(int i=start;i<n;i++){
@@ -21,4 +22,22 @@ class Solution {
             }
             return cnt;
         }
+        //this approach is optimised approach
+        class Solution1 {
+            public:
+            
+                int minimumOperations(vector<int>& nums) {
+                    int cnt=0;
+                    int n=nums.size();
+                    unordered_set<int> st;
+                    for(int i=n-1;i>0;i--){
+                        if(st.count(nums[i])){
+                           cnt=ceil((i+1)/3.0);
+                           return cnt;
+                        }
+                        st.insert(nums[i]);
+                    }
+                    return cnt;
+                }
+            };
     };
