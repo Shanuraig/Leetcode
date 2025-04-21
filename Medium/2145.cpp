645c31f7d562655596db912460e532a598c32bdf
Gyan Prakash Rai <shanuraig@gmail.com>
2025-04-21 13:35:35 +0530

    2145 medium

---
+++ Medium/2145.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+     int numberOfArrays(vector<int>& differences, int lower, int upper) {
+       long prefix = 0;
+       long mn = 0;  // Starts from 0.
+       long mx = 0;  // Starts from 0.
+   
+       for (const int d : differences) {
+         prefix += d;
+         mn = min(mn, prefix);
+         mx = max(mx, prefix);
+       }
+       return max(0L, (upper - lower) - (mx - mn) + 1);
+     }
+   };
