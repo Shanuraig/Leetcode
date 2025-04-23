c9f77f76baf0dd9f484834ceb70fe19c5afaf442
Gyan Prakash Rai <shanuraig@gmail.com>
2025-04-23 18:21:32 +0530

    1399 easy

---
+++ Easy/1399.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+//leetcode 1399
+// https://leetcode.com/problems/count-largest-group/description/
+class Solution {
+    public:
+        int countLargestGroup(int n) {
+            unordered_map<int,int>mp;
+            int sum=0,j=0;
+            for(int i=1;i<=n;i++){
+                sum=0,j=i;
+                while(j){
+                    sum+=j%10;
+                    j/=10;
+                }
+                mp[sum]++;
+            }
+            int ans=0,maxi=0;
+            for(auto &it:mp){
+                if(it.second>maxi){
+                    ans=1;
+                    maxi=it.second;
+                }
+                else if(it.second==maxi) ans++;
+            }
+            return ans;
+        }
+    };
