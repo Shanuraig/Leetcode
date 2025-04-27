340e450906c089cfe6ea5febfcb69218dba00f61
Gyan Prakash Rai <shanuraig@gmail.com>
2025-04-28 01:07:08 +0530

    3392 easy

---
+++ Easy/3392.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+        int countSubarrays(vector<int>& nums) {
+            int n=nums.size();
+            int count=0;
+            for(int i=1;i+1<n;i++){
+                if(nums[i]==(nums[i-1]+nums[i+1])*2){
+                    count++;
+                }
+            }
+            return count;
+        }
+    };
